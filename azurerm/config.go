a4eb4229fcb52bb683c20ccdb3da8e4b09e1b0d2
tombuildsstuff <git@ibuildstuff.co.uk>
2018-01-15 12:23:58 +0000

    Removing the unused scheduler config

--- azurerm/config.go
+++ azurerm/config.go
@@ -33,7 +33,6 @@ import (
 	"github.com/Azure/azure-sdk-for-go/arm/resources/resources"
 	"github.com/Azure/azure-sdk-for-go/arm/resources/subscriptions"
 	keyVault "github.com/Azure/azure-sdk-for-go/dataplane/keyvault"
-	"github.com/Azure/azure-sdk-for-go/services/scheduler/mgmt/2016-03-01/scheduler"
 	"github.com/Azure/azure-sdk-for-go/services/search/mgmt/2015-08-19/search"
 	"github.com/Azure/azure-sdk-for-go/services/servicebus/mgmt/2017-04-01/servicebus"
 	"github.com/Azure/azure-sdk-for-go/services/sql/mgmt/2015-05-01-preview/sql"
@@ -153,10 +152,6 @@ type ArmClient struct {
 
 	// Resources
 	managementLocksClient locks.ManagementLocksClient
-
-	// Scheduler
-	jobsClient            scheduler.JobsClient
-	jobsCollectionsClient scheduler.JobCollectionsClient
 
 	// Search
 	searchServicesClient search.ServicesClient
@@ -612,7 +607,6 @@ func getArmClient(c *authentication.Config) (*ArmClient, error) {
 	client.registerNetworkingClients(endpoint, c.SubscriptionID, auth, sender)
 	client.registerRedisClients(endpoint, c.SubscriptionID, auth, sender)
 	client.registerResourcesClients(endpoint, c.SubscriptionID, auth, sender)
-	client.registerSchedulerClients(endpoint, c.SubscriptionID, auth, sender)
 	client.registerSearchClients(endpoint, c.SubscriptionID, auth, sender)
 	client.registerServiceBusClients(endpoint, c.SubscriptionID, auth, sender)
 	client.registerStorageClients(endpoint, c.SubscriptionID, auth, sender)
@@ -852,22 +846,6 @@ func (c *ArmClient) registerResourcesClients(endpoint, subscriptionId string, au
 	locksClient.Sender = sender
 	locksClient.SkipResourceProviderRegistration = c.skipProviderRegistration
 	c.managementLocksClient = locksClient
-}
-
-func (c *ArmClient) registerSchedulerClients(endpoint, subscriptionId string, auth autorest.Authorizer, sender autorest.Sender) {
-	jobsClient := scheduler.NewJobsClientWithBaseURI(endpoint, c.subscriptionId)
-	setUserAgent(&jobsClient.Client)
-	jobsClient.Authorizer = auth
-	jobsClient.Sender = sender
-	jobsClient.SkipResourceProviderRegistration = c.skipProviderRegistration
-	c.jobsClient = jobsClient
-
-	collectionsClient := scheduler.NewJobCollectionsClientWithBaseURI(endpoint, subscriptionId)
-	setUserAgent(&collectionsClient.Client)
-	collectionsClient.Authorizer = auth
-	collectionsClient.Sender = sender
-	jobsClient.SkipResourceProviderRegistration = c.skipProviderRegistration
-	c.jobsCollectionsClient = collectionsClient
 }
 
 func (c *ArmClient) registerSearchClients(endpoint, subscriptionId string, auth autorest.Authorizer, sender autorest.Sender) {
